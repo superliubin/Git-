adc0a83573a53b8ff6e0ab8a93ee02fc3c9a10cb
liubin <3468059492@qq.com>
2019-12-24 10:22:34 +0800

    项目信息

---
+++ src/main/java/com/provider/controller/TicketController.java
@@ -0,0 +1,19 @@
+package com.provider.controller;
+
+import com.provider.service.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TicketController {
+
+    @Autowired
+    TicketService ticketService;
+
+    @GetMapping("/ticket")
+    public String getTicket(){
+        System.out.println("服务提供");
+        return ticketService.getTicket();
+    }
+}
